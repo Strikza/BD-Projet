6f808e6075003bce779fd02a13bdd6a1f220543d
Louis Forestier <louis.forestier@etu.univ-poitiers.fr>
2021-04-19 03:56:02 +0200

    mise en ordre des créations de tables et ajout des clés primaires et étrangères

--- create_tables.sql
+++ create_tables.sql
@@ -2,20 +2,96 @@ CREATE TABLE Document (
   id int,
   title varchar2(100),
   id_publisher int,
-  theme varchar2(50)
+  theme varchar2(50),
+  constraint PK_Document primary key (id),
+  constraint FK_Document_Publisher foreign key (id_publisher) references Publisher(id)
+  constraint FK_Document_Theme foreign key (theme) references Theme(name)
 );
 
 CREATE TABLE Publisher (
   id int,
   name varchar2(100),
   address varchar2(100),
-  phone varchar2(15)
+  phone varchar2(15),
+  constraint PK_Publisher primary key (id)
+);
+
+CREATE TABLE Author (
+  id int,
+  last_name varchar2(100),
+  first_name varchar2(100),
+  birthday date,
+  constraint PK_Author primary key (id)
+);
+
+CREATE TABLE AuthorDocument (
+  id_document int,
+  id_author int,
+  constraint PK_AuthorDocument primary key (id_document, id_author),
+  constraint FK_AuthorDocument_Author foreign key (id_author) references Author(id)
+  constraint FK_AuthorDocument_Document foreign key (id_document) references Document(id)
+);
+
+CREATE TABLE Keyword (
+  name varchar2(50),
+  constraint PK_Keyword primary key (name)
+);
+
+CREATE TABLE KeywordDocument (
+  Keyword_name varchar2(50),
+  id_document int,
+  constraint PK_KeywordDocument primary key (Keyword_name, id_document),
+  constraint FK_KeywordDocument_Keyword foreign key (keyword_name) references Keyword(name),
+  constraint FK_KeywordDocument_Document foreign key (id_document) references Document(id)
+);
+
+CREATE TABLE Theme (
+  name varchar2(50),
+  constraint PK_Theme primary key (name)
+);
+
+CREATE TABLE Book (
+  id_document int,
+  pages_nb int,
+  constraint PK_Book primary key (id_document),
+  constraint FK_Book_Document foreign key (id_document) references Document(id)
+);
+
+CREATE TABLE CD (
+  id_document int,
+  duration time,
+  subtitles_nb int,
+  constraint PK_CD primary key (id_document),
+  constraint FK_CD_Document foreign key (id_document) references Document(id)
+);
+
+CREATE TABLE DVD (
+  id_document int,
+  duration time,
+  constraint PK_DVD primary key (id_document),
+  constraint FK_DVD_Document foreign key (id_document) references Document(id)
+);
+
+CREATE TABLE Video (
+  id_document int,
+  duration time,
+  format varchar2(5),
+  constraint PK_Video primary key (id_document),
+  constraint FK_Video_Document foreign key (id_document) references Document(id)
 );
 
 CREATE TABLE Copy (
   id int,
   id_document int,
-  shelf_num int
+  shelf_num int,
+  constraint PK_Copy primary key (id),
+  constraint FK_Copy_Document foreign key (id_document) references Document(id)
+);
+
+CREATE TABLE Shelf (
+  shelf_num int,
+  remaining_slots int,
+  constraint PK_Shelf primary key (shelf_num)
 );
 
 CREATE TABLE Borrower (
@@ -24,35 +100,19 @@ CREATE TABLE Borrower (
   first_name varchar2(100),
   address varchar2(100),
   phone varchar2(15),
-  borrower_type varchar2(100)
+  borrower_type varchar2(100),
+  constraint PK_Borrower primary key (id),
+  constraint FK_Borrower_Borrowertype foreign key (borrower_type) references Borrowertype(name)
 );
 
-CREATE TABLE Keyword (
-  name varchar2(50)
-);
-
-CREATE TABLE shelf (
-  shelf_num int,
-  remaining_slots int
-);
-
-CREATE TABLE Author_Document (
-  id_document int,
-  id_author int
-);
-
-CREATE TABLE Borrower_type (
+CREATE TABLE Borrowertype (
   name varchar2(100),
   max_borrow int,
   book_time int,
   cd_time int,
   dvd_time int,
-  video_time int
-);
-
-CREATE TABLE Book (
-  id_document int,
-  pages_nb int
+  video_time int,
+  constraint PK_Borrower_type primary key (name)
 );
 
 CREATE TABLE Borrow (
@@ -60,38 +120,14 @@ CREATE TABLE Borrow (
   id_borrower int,
   borrowed_date date,
   max_return_date date,
-  return_date date
+  return_date date,
+  constraint PK_Borrow primary key (id_copy, id_borrower, borrowed_date),
+  constraint FK_Borrow_Copy foreign key (id_copy) references Copy(id),
+  constraint FK_Borrow_Borrower foreign key (id_borrower) references Borrower(id)
 );
 
-CREATE TABLE CD (
-  id_document int,
-  duration time,
-  subtitles_nb int
-);
 
-CREATE TABLE Author (
-  id int,
-  last_name varchar2(100),
-  first_name varchar2(100),
-  birthday date
-);
 
-CREATE TABLE Keyword_Document (
-  Keyword_name varchar2(50),
-  id_document int
-);
 
-CREATE TABLE Video (
-  id_document int,
-  duration time,
-  format varchar2(5)
-);
 
-CREATE TABLE Theme (
-  name varchar2(50)
-);
 
-CREATE TABLE DVD (
-  id_document int,
-  duration time
-);
