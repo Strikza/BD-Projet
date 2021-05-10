346af23bfb6154f999cbbc3e36815c1eba786f3f
cguill18 <clementineguillot@gmail.com>
2021-05-10 17:01:41 +0200

    update transaction

--- transactions.sql
+++ transactions.sql
@@ -4,7 +4,7 @@ SET AUTOCOMMIT OFF;
 -- transaction dans borrower
 
 -- ajout
-LOCK TABLE borrower IN SHARE ROW EXCLUSIVE MODE
+LOCK TABLE borrower IN SHARE MODE
 insert into borrower(last_name,first_name,address,phone,borrower_type)
 values ('nom','prenom','address',0011223344,'PUBLIC');
 commit;
@@ -19,20 +19,29 @@ commit;
 -- transaction dans document
 
 -- ajout
-lock table document in share row exclusive mode;
+savepoint ajout_doc;
+lock table document in share mode;
+-- insert document
+-- insert book
+-- insert authorducument
+-- si probleme rollback to ajout_doc
+commit;
 
 -- suppression 
 select *
 from document 
 where id = 1 for update;
+commit;
 
 -- transaction dans borrow
 -- ajout
-lock table borrow in share row exclusive mode;
+lock table borrow in share mode;
+commit;
 
 -- suppression 
 select *
 from borrow 
 where id = 1 for update;
+commit;
 
 
