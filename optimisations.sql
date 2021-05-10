cbc756c1e72aa22c574dd092e95755ec59ab51f2
Goubeau Samuel <sams86240@gmail.com>
2021-05-10 16:51:19 +0200

    Add optimisations

--- optimisations.sql
+++ optimisations.sql
@@ -2,6 +2,12 @@
 -- le thème doit contenir informatique donc il faut une wildcard au début et à la fin. 
 -- donc on ne peut pas optimiser la requête avec un index
 
+-- Q.2
+-- On créé 2 index. L'un sur l'attribut last_name de Borrower, et l'autre sur l'attribut borrowed_date de Borrow
+
+-- Q.3
+-- Aucune optimisation. Mais il est possible de créer une vue concrète, pour calculer parcourir une seul fois les tables
+
 -- Q.4
 create index publisher_name on publisher(upper(name)); 
 
@@ -12,11 +18,20 @@ create index copy_id_doc on copy(id_document);
 -- Q.6
 -- pas d'optimisations possibles
 
+-- Q.7
+-- Pas d'optimisation
+
 -- Q.8
 create bitmap index bidx_document_theme on document(theme);
 
+-- Q.9
+-- On reprend l'index sur l'attribut last_name de la requête n°2, et on en créé un autre sur l'adresse de Borrower
+
 -- Q.10
 -- reutilisation de l'index sur le theme des document
+
+-- Q.11
+-- Pas d'optimisation
 
 -- Q.12
 -- rien
@@ -28,6 +43,9 @@ create bitmap index bidx_document_type on document(upper(type));
 -- Q.14
 -- faire une vue concrete sur le count
 
+-- Q.15
+-- On créé un index bitmap sur les thèmes
+
 -- Q.16
 -- faire une vue sur le count ?
 
@@ -38,4 +56,7 @@ create index idx_document_title on document(title);
 -- réutilisation de l'index sur le titre
 
 -- Q.19
+-- réutilisation de l'index sur le titre
+
+-- Q.20
 -- réutilisation de l'index sur le titre
